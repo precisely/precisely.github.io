1696b244ba3eb057dbc44839345088b480588698
Andrew Wang <andrewxiaoyu0@gmail.com>
2022-07-05 13:45:21 -0400

    Add X on Member Modal

--- src/lib/about/TeamBanner/MemberModal.svelte
+++ src/lib/about/TeamBanner/MemberModal.svelte
@@ -2,6 +2,7 @@
 	import Container from '$lib/components/Container.svelte';
 	import { hideComponent } from '$lib/components/ModalWrapper/ModalWrapper.svelte';
 	import MemberData from './MemberData.svelte';
+	import XmarkSvg from './xmark.svg';
 	import './memberModal.css';
 
 	export let src: string;
@@ -11,7 +12,13 @@
 </script>
 
 <Container>
-	<div class="bg-white flex flex-col md:flex-row p-8 gap-8 rounded-lg" on:click={hideComponent}>
+	<div
+		class="bg-white relative flex flex-col md:flex-row pt-16 p-8 gap-8 rounded-lg"
+		on:click={hideComponent}
+	>
+		<div class="absolute top-4 right-4 cursor-pointer">
+			<XmarkSvg height="24px" />
+		</div>
 		<div class="flex flex-col items-center">
 			<MemberData {src} {name} {title} />
 		</div>
